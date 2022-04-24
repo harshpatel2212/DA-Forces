477c8a22528ce4efecda74fec4049b672486bb81
harshpatel2212 <harshup2212@gmail.com>
2022-04-25 01:53:53 +0530

    Added More Features and Added Struct Changes

--- Admin/sign_up.c
+++ Admin/sign_up.c
@@ -1,34 +1,52 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
+#include <conio.h>
+#include <windows.h>
+#include <process.h>
+#include <direct.h>
+
+#define MAXUSERSIZE 100
+
+typedef struct usersDataBase
+{
+    int id;
+    char name[50];
+    char password[50];
+} user;
 
 int main()
 {
+    int number_of_users = 0;
+    user users[MAXUSERSIZE];
     system("cls");
-    FILE* ptr;
+    FILE *ptr;
+launch:
     printf("Welcome To DA_Forces\n\n"
-    
-            "Select the appropriate choice from following\n\n"
-            
-            "1) Enter 0 for User\n"
-            "2) Enter 1 for Admin\n\n");
 
-    int  choice_1;
+           "Select the appropriate choice from following\n\n"
+
+           "1) Enter 0 for User\n"
+           "2) Enter 1 for Admin\n\n");
+
+    int choice_1;
     scanf("%d", &choice_1);
     system("cls");
-    if(choice_1 == 0)
+    if (choice_1 == 0)
     {
         printf("Select the appropriate choice from following\n\n"
-            
-            "1) Enter 0 for Login\n"
-            "2) Enter 1 for Signup\n\n");
+
+               "1) Enter 0 for Login\n"
+               "2) Enter 1 for Signup\n\n");
 
         int choice_2;
         scanf("%d", &choice_2);
 
         system("cls");
 
-        if(choice_2 == 0)
+        if (choice_2 == 0)
         {
             char us_id[50], password[50];
             printf("Username : ");
@@ -37,14 +55,12 @@ int main()
             scanf("%s", password);
             ptr = fopen("users_data.txt", "r");
             char temp[50];
-            while(fscanf(ptr, "%s", temp)!=EOF)
+            while (fscanf(ptr, "%s", temp) != EOF)
             {
-                printf("\n%s", temp);
-                if(strcmp(temp, us_id) == 0)
+                if (strcmp(temp, us_id) == 0)
                 {
                     fscanf(ptr, "%s", temp);
-                    printf("\n%s", temp);
-                    if(strcmp(temp, password) == 0)
+                    if (strcmp(temp, password) == 0)
                     {
                         printf("\n\nSucessfully Login");
                         break;
@@ -56,39 +72,74 @@ int main()
                 }
             }
             fclose(ptr);
-            //confirm Username and password from file
         }
         else
         {
-            char us_id[50], password[50];
+            char us_id[50], password[50], confmPass[50];
+        username:
             printf("Username : ");
             scanf("%s", us_id);
-            printf("Password : ");
-            scanf("%s", password);
-            //confirm password
-            //username already exists
             ptr = fopen("users_data.txt", "r");
-            for(int i=0;i != EOF; i++)
+            for (int i = 0; i != EOF; i++)
             {
                 char temp[50];
                 fscanf(ptr, "%s", temp);
-                if(strcmp(temp, us_id) == 0)
+                if (strcmp(temp, us_id) == 0)
                 {
-                    fscanf(ptr, "%s", temp);
-                    if(temp == password)
-                    {
-                        printf("Username already exists\n\n");
-                        break;
-                    }
+                    printf("Username already exists\n\n");
+                    printf("Choose a unique Username\n\n");
+                    goto username;
                 }
             }
             fclose(ptr);
-            //Add username and password in file
-            ptr = fopen("users_data.txt", "a");
-            fprintf(ptr, "%s %s\n", us_id, password);
-            fclose(ptr);
+            printf("Password : ");
+            scanf("%s", password);
+        confirmPassword:
+            printf("Confirm Password : ");
+            scanf("%s", confmPass);
+            if (confmPass == password)
+            {
+                ptr = fopen("users_data.txt", "a");
+                fprintf(ptr, "%s %s\n", us_id, password);
+                fclose(ptr);
+                printf("\n\nSucessfully Signup");
+                users[number_of_users].id = number_of_users;
+                strncpy(users[number_of_users].name, us_id, 50);
+                users[number_of_users].name[49] = '\0';
+                strncpy(users[number_of_users].password, password, 50);
+                users[number_of_users].password[49] = '\0';
+                number_of_users++;
+            }
+            else
+            {
+                printf("\n\nPassword and Confirm Password doesn't match");
+                goto confirmPassword;
+            }
+            goto launch;
         }
     }
+    else
+    {
+        printf("Select the appropriate choice from following\n\n"
 
+               "1) Enter 0 for Shut-Down System\n"
+               "2) Enter 1 for Adding Question\n\n");
+
+        int choice_3;
+        scanf("%d", &choice_3);
+
+        system("cls");
+        if (choice_3)
+        {
+            // Add Question
+        }
+        else
+        {
+            // Shut-Down System
+            printf("\nShutting Down System...\n\n");
+            Sleep(1000);
+            exit(0);
+        }
+    }
     return 0;
 }
